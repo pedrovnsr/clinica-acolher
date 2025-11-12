2f56c9544b2bdf527bf6bda6243c7728b589b092
Feliipee013 <ftaveira611@gmail.com>
2025-11-12 19:14:30 -0300

    Feat: Conexao com o banco da criacao de usuarios com ou sem responsavel

--- src/controller/api.tsx
+++ src/controller/api.tsx
@@ -11,3 +11,19 @@ export function professionalRegistration(payload : object) {
         }
     });
 }
+
+export function patientRegistration(payload : object) {
+    return axios.post("http://localhost:8080/api/patients", payload, {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        }
+    });
+}
+
+export function patientGuardianRegistration(payload : object) {
+    return axios.post("http://localhost:8080/api/guardians", payload, {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        }
+    });
+}
